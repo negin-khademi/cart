56bf8229928aaba38aa2667e12ab80af8a8accf5
Negin <negin.khademi@gmail.com>
2025-01-05 19:09:36 +0330

    initial project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import CartContainer from './CartContainer';
+// components
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+
+export default App;
